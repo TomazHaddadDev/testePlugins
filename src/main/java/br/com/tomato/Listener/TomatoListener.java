470ae6e224f6f5e8480e77826c799fa3c9667663
TomazHaddadDev <tomazhaddad@gmail.com>
2024-11-11 14:52:56 -0300

    Fireball Plugin added

--- src/main/java/br/com/tomato/Listener/TomatoListener.java
+++ src/main/java/br/com/tomato/Listener/TomatoListener.java
@@ -3,6 +3,7 @@ package br.com.tomato.Listener;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -105,6 +106,28 @@ public class TomatoListener implements Listener {
 
     }
 
+    //Solta bola de fogo pela blazerod
+    @EventHandler
+    public void fireball(PlayerInteractEvent event){
+
+
+
+        if(event.getAction() == Action.RIGHT_CLICK_AIR){
+
+            Player player = event.getPlayer();
+            Material material = event.getMaterial();
+
+            if(material == Material.BLAZE_ROD){
+                Fireball fireball = player.launchProjectile(Fireball.class);
+                fireball.setVelocity(player.getLocation().getDirection().multiply(1.5));
+                player.sendMessage(ChatColor.DARK_RED + " bola de fogo lançada");
+
+            }
+
+
+        }
+
+    }
 
 
 
